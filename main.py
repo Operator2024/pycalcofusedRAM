2f4e3c92af1074a6f1d8211eaed10ec7c5cad584
Vladimir Belomestnykh <work.pwnz@gmail.com>
2019-03-20 11:30:05 +0500

    Version 1.0.0
    Removed unnecessary code.
    Added full support RU (code 0419) and EN (code 409) locale.
    Code changed in accordance with PEP8

--- main.py
+++ main.py
@@ -1,40 +1,40 @@
 # -*- coding: utf-8 -*-
+# Version 1.0.0
 
-import os,sys,subprocess,time
-import re
-
+from subprocess import Popen, PIPE
+from re import sub
+from time import time
 
 variab = input("Enter process name as 'tasklist': ")
-proc = subprocess.Popen(["tasklist",'/FI',"IMAGENAME eq {0}".format(variab),"/FO","TABLE","/NH"],stdout=subprocess.PIPE,shell=True)
+proc = Popen(["tasklist", '/FI', "IMAGENAME eq {0}".format(variab), "/FO", "TABLE", "/NH"], stdout=PIPE, shell=True)
 data = proc.stdout.readlines()
 clearNum = 0
 
-for i in range(1,len(data)):
-    print(re.sub(r"\s+", "||", data[i].decode("cp866").replace("\s", "")).split("||"))
-    # получаем число в виде строки и убираем запятую.
-    if( ("KB" in re.sub(r"\s+", "||", data[i].decode("cp866").replace("\s", "")).split("||")[5] or "КБ" in re.sub(r"\s+", "||", data[i].decode("cp866").replace("\s", "")).split("||")[5])
-        and (re.sub(r"\s+", "||", data[i].decode("cp866").replace("\s", "")).split("||")[5].isdigit() != True)):
+for i in range(1, len(data)):
+    # ==================================================================
+    # Пробегаем по всем элементам тасклиста и обрабатываем вывод.
+    # После обработки суммируем и записываем в файл, который создается в дериктории запуска скрипта.
+    # ==================================================================
+    # In english
+    # After receiving a list of a task begun processing him.
+    # Then summarize and write all to file, which will be created in the directory is running this script.
+    # ==================================================================
 
-        Num = re.sub(r"\s+", "||", data[i].decode("cp866").replace("\s", "")).split("||")[4]
-        clearNum +=int(re.sub(r",","",Num))
+    if(("KB" in sub(r"\s+", "||", data[i].decode("cp866").replace(r"\s", "")).split("||")[5] or "КБ" in sub(r"\s+", "||", data[i].decode("cp866").replace(r"\s", "")).split("||")[5] or "K" in sub(r"\s+", "||", data[i].decode("cp866").replace(r"\s", "")).split("||")[5])
+            and (sub(r"\s+", "||", data[i].decode("cp866").replace(r"\s", "")).split("||")[5].isdigit() != True)):
+
+        Num = sub(r"\s+", "||", data[i].decode("cp866").replace(r"\s", "")).split("||")[4]
+        clearNum += int(sub(r",", "", Num))
     else:
-        Num = re.sub(r"\s+", "||", data[i].decode("cp866").replace("\s", "")).split("||")[4] + re.sub(r"\s+", "||", data[i].decode("cp866").replace("\s", "")).split("||")[5]
-        clearNum += int(re.sub(r",", "", Num))
+        Num = sub(r"\s+", "||", data[i].decode("cp866").replace(r"\s", "")).split("||")[4] + \
+              sub(r"\s+", "||", data[i].decode("cp866").replace(r"\s", "")).split("||")[5]
+        clearNum += int(sub(r",", "", Num))
 
-
-with open(f'{variab[0:len(variab)-4]}.txt',"w") as file:
+with open(f'{variab[0:len(variab) - 4]}.txt', "w") as file:
     file.write("Memory is busy process: {0} bytes, {1} megabytes, {2} kilobytes \nTotal process: {3}".format(
-        clearNum * 1024,clearNum/1024, clearNum,len(data)-1))
+        clearNum * 1024, clearNum / 1024, clearNum, len(data) - 1))
 
-print("Memory is busy process: {0} bytes, {1} megabytes, {2} kilobytes".format(clearNum * 1024,clearNum/1024, clearNum))
-
-# c = wmi.WMI ()
-# allocateMemory = []
-# for process in c.Win32_Process (Name="epic.exe".format(input('Enter process name as "tasklist": '))):
-#
-#     allocateMemory.append(int(process.WorkingSetSize))
-#
-# print("Memory is busy process: {0} bytes, {1} megabytes, {2} kilobytes".format(sum(allocateMemory),sum(allocateMemory)/1048576,sum(allocateMemory)/1024))
-
-time.sleep(10)
-
+print("Memory is busy process: {0} bytes, {1} megabytes, {2} kilobytes".format(clearNum * 1024, clearNum / 1024,
+                                                                               clearNum))
+# for debug string
+# time.sleep(3)
